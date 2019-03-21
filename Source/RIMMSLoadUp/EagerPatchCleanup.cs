4217d5c612b109b5f61481dc2b9ac3b904c05467
razuhl <48775151+razuhl@users.noreply.github.com>
2019-03-21 17:56:47 +0100

    Eager cleanup for xml patches
    Instead of holding the resources for xml patches until all mods have finished their patches, the resources are freed up once their corresponding mod has processed all its patches.

---
+++ Source/RIMMSLoadUp/EagerPatchCleanup.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Malte Schulze
+ * Date: 21.03.2019
+ * Time: 17:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Harmony;
+using Verse;
+
+namespace RIMMSLoadUp
+{
+	/// <summary>
+	/// Description of EagerPatchCleanup.
+	/// </summary>
+	[HarmonyPatch(typeof(Verse.LoadedModManager))]
+	[HarmonyPatch("ApplyPatches")]
+	static class ApplyPatchesPatch {
+		static bool Prefix(XmlDocument xmlDoc, Dictionary<XmlNode, LoadableXmlAsset> assetlookup) {
+			foreach (ModContentPack mcp in LoadedModManager.RunningMods) {
+				foreach ( PatchOperation po in mcp.Patches ) {
+					try {
+						po.Apply(xmlDoc);
+					} catch (Exception arg) {
+						Log.Error("Error in patch.Apply(): " + arg, false);
+					}
+				}
+				foreach ( PatchOperation po in mcp.Patches ) {
+					try {
+						po.Complete(mcp.Name);
+					} catch (Exception arg) {
+						Log.Error("Error in patch.Complete(): " + arg, false);
+					}
+				}
+				mcp.ClearPatchesCache();
+			}
+			
+			return false;
+		}
+	}
+	
+	[HarmonyPatch(typeof(Verse.LoadedModManager))]
+	[HarmonyPatch("ClearCachedPatches")]
+	static class ClearCachedPatchesPatch {
+		static bool Prefix() {
+			return false;
+		}
+	}
+}
