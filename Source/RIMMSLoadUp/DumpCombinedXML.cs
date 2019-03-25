96e8355917fe95aa79b98730d8d8f3862aa3e63c
razuhl <48775151+razuhl@users.noreply.github.com>
2019-03-25 16:23:25 +0100

    Prep for next release
    Inactive patches for dumping combined xml and ignoring comments/whitespaces when reading xml.

---
+++ Source/RIMMSLoadUp/DumpCombinedXML.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Malte Schulze
+ * Date: 25.03.2019
+ * Time: 14:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Harmony;
+using Verse;
+
+namespace RIMMSLoadUp
+{
+	//[HarmonyPatch(typeof(Verse.LoadedModManager))]
+	//[HarmonyPatch("ParseAndProcessXML")]
+	static class DumpCombinedXML
+	{
+		static void Postfix(XmlDocument xmlDoc)
+		{
+			SaveXMLToFile("combinedXml.xml", xmlDoc);
+		}
+		
+		static void SaveXMLToFile(string fileName, XmlDocument xml) {
+			FileInfo file = new FileInfo(Path.Combine(GenFilePaths.SaveDataFolderPath, fileName));
+			try {
+				using (var writer = XmlWriter.Create(file.OpenWrite(), new XmlWriterSettings{Indent = false,OmitXmlDeclaration = true,NewLineHandling = NewLineHandling.Replace}))
+				{
+					xml.WriteTo(writer);
+				}
+			} catch (Exception e) {
+				Log.Error("Failed to save xml to file \""+file.FullName+"\"! " + e);
+			}
+		}
+	}
+}
---
+++ Source/RIMMSLoadUp/IgnoreCommentsInXML.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Malte Schulze
+ * Date: 25.03.2019
+ * Time: 15:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Xml;
+using Harmony;
+using Verse;
+
+namespace RIMMSLoadUp
+{
+	//[HarmonyPatch(typeof(LoadableXmlAsset))]
+	//[HarmonyPatch(MethodType.Constructor)]
+	//[HarmonyPatch(new Type[] {typeof(string),typeof(string),typeof(string)})]
+	static class IgnoreCommentsInXML
+	{
+		static bool Prefix(LoadableXmlAsset __instance, string name, string fullFolderPath, string contents)
+		{
+			__instance.name = name;
+			__instance.fullFolderPath = fullFolderPath;
+			try {
+				using(XmlReader r = XmlReader.Create(new StringReader(contents),new XmlReaderSettings(){IgnoreComments = true, IgnoreWhitespace = true})) {
+					__instance.xmlDoc = new XmlDocument();
+					__instance.xmlDoc.Load(r);
+				}
+			} catch (Exception ex) {
+				Log.Warning(string.Concat(new object[]
+				{
+					"Exception reading ",
+					name,
+					" as XML: ",
+					ex
+				}), false);
+				__instance.xmlDoc = null;
+			}
+			return false;
+		}
+	}
+	
+	//[HarmonyPatch(typeof(ScribeLoader))]
+	//[HarmonyPatch("InitLoading")]
+	static class IgnoreCommentsInXMLFromScribeLoader {
+		static bool Prefix(ScribeLoader __instance, string filePath) {
+			if (Scribe.mode != LoadSaveMode.Inactive)
+			{
+				Log.Error("Called InitLoading() but current mode is " + Scribe.mode, false);
+				Scribe.ForceStop();
+			}
+			if (__instance.curParent != null)
+			{
+				Log.Error("Current parent is not null in InitLoading", false);
+				__instance.curParent = null;
+			}
+			if (__instance.curPathRelToParent != null)
+			{
+				Log.Error("Current path relative to parent is not null in InitLoading", false);
+				__instance.curPathRelToParent = null;
+			}
+			try
+			{
+				using (StreamReader streamReader = new StreamReader(filePath))
+				{
+					using(XmlReader r = XmlReader.Create(streamReader,new XmlReaderSettings(){IgnoreComments = true, IgnoreWhitespace = true})) {
+						XmlDocument xmlDocument = new XmlDocument();
+						xmlDocument.Load(r);
+						__instance.curXmlParent = xmlDocument.DocumentElement;
+					}
+				}
+				Scribe.mode = LoadSaveMode.LoadingVars;
+			}
+			catch (Exception ex)
+			{
+				Log.Error(string.Concat(new object[]
+				{
+					"Exception while init loading file: ",
+					filePath,
+					"\n",
+					ex
+				}), false);
+				__instance.ForceStop();
+				throw;
+			}
+			
+			return false;
+		}
+	}
+}
